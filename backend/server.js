7cd7abbe33147f6cea6e05e7e66c9669c6958c65
Vando Costa <da.vandsc@gmail.com>
2026-03-20 13:58:12 -0300

    fix: corrige cors para frontend vercel

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,7 @@
-require("dotenv").config()
-
-const express = require("express")
 const cors = require("cors")
-const connectDB = require("./config/db")
 
-const authRoutes = require("./routes/authRoutes")
-const appointmentRoutes = require("./routes/appointmentRoutes")
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-connectDB()
-
-app.use("/api/auth", authRoutes)
-app.use("/api/appointments", appointmentRoutes)
-
-app.get("/", (req, res) => {
-  res.send("API da clínica funcionando")
-})
-
-const PORT = process.env.PORT || 4000
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`)
-})
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}))
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api"
+  baseURL: 
+  "https://sistema-agendamento-m-dico.onrender.com/api"
 })
 
 api.interceptors.request.use((config) => {
