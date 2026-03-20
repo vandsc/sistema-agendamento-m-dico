1caa9e8475ec32dc8229a1373e0849c9df11c3d0
Vando Costa <da.vandsc@gmail.com>
2026-03-20 14:18:55 -0300

    fix: corrige Register.vue

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,18 +1,45 @@
-async register() {
-  try {
-    await api.post("/auth/register", {
-      name: this.name,
-      email: this.email,
-      password: this.password
-    })
+<template>
+  <div>
+    <h2>Cadastro</h2>
 
-    this.message = "Cadastro realizado com sucesso"
-    this.$router.push("/login")
-  } catch (error) {
-    console.log(error.response?.data || error)
-    this.message =
-      error.response?.data?.message ||
-      error.response?.data?.error ||
-      "Erro ao cadastrar"
+    <input v-model="name" placeholder="Nome" />
+    <input v-model="email" placeholder="Email" />
+    <input v-model="password" type="password" placeholder="Senha" />
+
+    <button @click="register">Cadastrar</button>
+
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import api from "../services/api"
+
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      message: ""
+    }
+  },
+  methods: {
+    async register() {
+      try {
+        await api.post("/auth/register", {
+          name: this.name,
+          email: this.email,
+          password: this.password
+        })
+
+        this.message = "Cadastro realizado com sucesso"
+        this.$router.push("/login")
+      } catch (error) {
+        console.error(error)
+        this.message = "Erro ao cadastrar"
+      }
+    }
   }
 }
+</script>
